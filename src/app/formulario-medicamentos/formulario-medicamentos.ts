a8a18e90eb87f9377269c1441011a1bd328b1a03
AnaVetericano <anyta822@gmail.com>
2026-08-28 22:53:51 -0500

    Feat: actualizacion componentes

--- src/app/formulario-medicamentos/formulario-medicamentos.ts
+++ src/app/formulario-medicamentos/formulario-medicamentos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-formulario-medicamentos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './formulario-medicamentos.html',
   styleUrl: './formulario-medicamentos.css',
 })
