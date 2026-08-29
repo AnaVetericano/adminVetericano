6367c575e999c650ceb21142ffb963d030492f70
jhormanquina17-arch <jhormanquina17@gmail.com>
2026-08-29 08:35:42 -0500

    patologias}

---
+++ src/app/crear-pa/crear-pa.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-pa',
+  imports: [],
+  templateUrl: './crear-pa.html',
+  styleUrl: './crear-pa.css',
+})
+export class CrearPa {}
