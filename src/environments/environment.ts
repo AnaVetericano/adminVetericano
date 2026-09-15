3ef8d79bb5b07420bd4cc10cda02674df880e610
Froshi <125601800+Froshi-iuri@users.noreply.github.com>
2026-09-15 03:50:10 -0500

    Fix merge conflict in environment.ts
    Removed conflicting apiUrlMedicamentos entry.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://backendvetericano-production.up.railway.app/api/usuarios',
   apiUrlespecies: 'https://backendvetericano-production.up.railway.app/api/especies',
-<<<<<<< HEAD
   apiUrlMedicamentos: 'https://backendvetericano-production.up.railway.app/api/medicamentos/'
-=======
->>>>>>> 8dce0211a31ce651589782082791a4d82f3faa64
 };
